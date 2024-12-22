8cc279fb17f1d66e16fcc7e511541c164079b36b
Developer Ujjal <ujjalchandra213@gmail.com>
2024-12-22 16:47:38 +0600

    add footer section

--- bistro-boss-restaurant-client/src/Components/Root/Root.jsx
+++ bistro-boss-restaurant-client/src/Components/Root/Root.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router";
 import Header from "../../Pages/HomePage/Shared/Header/Header";
+import Footer from "../../Pages/HomePage/Shared/Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <Header />
-            <div>
+            <div className="h-screen">
                 <Outlet />
             </div>
+            <Footer />
 
         </div>
     );
