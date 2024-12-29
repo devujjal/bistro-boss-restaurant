2b38e50913bb72a59594632fb23476a8ad5df898
Developer Ujjal <ujjalchandra213@gmail.com>
2024-12-29 14:28:05 +0600

    make responsive the dashboard pages and menu

--- bistro-boss-restaurant-client/src/Layouts/Dashboard/Dashboard.jsx
+++ bistro-boss-restaurant-client/src/Layouts/Dashboard/Dashboard.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router";
 import AsideBar from "../../Pages/DashboardPage/AsideBar/AsideBar";
+import ResMenu from "./ResMenu";
+import { useState } from "react";
 
 const Dashboard = () => {
+    const [toggle, setToggle] = useState(false)
+
     return (
-        <div className="flex h-screen">
-            <div className="w-64 bg-gray-800 text-white">
-                <AsideBar />
+        <div className="flex flex-col md:flex-row h-screen">
+            <div className="block md:hidden">
+                <ResMenu setToggle={setToggle} />
+            </div>
+            <div className={`${toggle === true ? 'block' : 'hidden'} md:block w-64 z-10 fixed text-white`}>
+                <AsideBar setToggle={setToggle} />
             </div>
             <main className="flex-1">
                 <Outlet />
