915bba028ba87050207120aed02ae591ac4b88fc
Developer Ujjal <ujjalchandra213@gmail.com>
2024-12-31 17:56:05 +0600

    add form handler

--- bistro-boss-restaurant-client/src/Pages/AdminDashboardPage/AddItem/AddItem.jsx
+++ bistro-boss-restaurant-client/src/Pages/AdminDashboardPage/AddItem/AddItem.jsx
@@ -11,7 +11,7 @@ const AddItem = () => {
         <section>
             <div className="container mx-auto">
 
-                <div className=" relative m-10">
+                <div className=" relative m-10 pb-14">
                     <SectionTitle heading={'ADD AN ITEM'} subHeading={"What's new?"} />
 
                     <div className="flex justify-center">
@@ -21,7 +21,7 @@ const AddItem = () => {
                             <div className="space-y-4">
                                 <div>
                                     <label htmlFor="product-name" className="block mb-2 text-xl font-normal font-inter text-[#3e454c]">Recipe name*</label>
-                                    <input type="text" {...register('item_name')} id="product-name" className="shadow-sm border border-gray-300 text-gray-900 sm:text-base rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5 font-raj" placeholder="Enter your item name" required="" />
+                                    <input type="text" {...register('name')} id="product-name" className="shadow-sm border border-gray-300 text-gray-900 sm:text-base rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5 font-raj" placeholder="Enter your item name" required="" />
                                 </div>
 
                                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -48,7 +48,7 @@ const AddItem = () => {
 
                                 <div className="">
                                     <label htmlFor="product-details" className="block mb-2 text-xl font-normal font-inter text-[#3e454c]">Product Details</label>
-                                    <textarea id="product-details"{...register('details')} rows="6" className="border border-gray-300 text-gray-900 sm:text-base font-raj rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-4" placeholder="Details"></textarea>
+                                    <textarea id="product-details"{...register('recipe')} rows="6" className="border border-gray-300 text-gray-900 sm:text-base font-raj rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-4" placeholder="Details"></textarea>
                                 </div>
 
 
@@ -56,7 +56,7 @@ const AddItem = () => {
 
                             <div className="font-inter max-w-md">
                                 <label className="text-base text-gray-500 font-semibold mb-2 block">Upload file</label>
-                                <input type="file"
+                                <input type="file" {...register('image')}
                                     className="w-full text-gray-400 font-semibold text-sm bg-white border file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-gray-100 file:hover:bg-gray-200 file:text-gray-500 rounded" />
                                 <p className="text-xs text-gray-400 mt-2">PNG and JPG are Allowed.</p>
                             </div>
